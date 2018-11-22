9deb415ded93adb28edb5f38f4b5c565d96e65df
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-11-22 23:36:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -396,6 +396,29 @@ client.on('message', message => {
     }
 });
 
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('+bc-users')){
+if(!message.author.id === 'اي دي صاحب البوت') return;
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
