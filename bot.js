96f317f0c574253dd9fda761e2835c4bae281c5a
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-11-22 20:30:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -290,7 +290,7 @@ if(!message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return mess
 });
 
 client.on('message', message => {
-    var prefix = "+";
+    var prefix = "*";
    
         if (message.author.id === client.user.id) return;
         if (message.guild) {
@@ -303,6 +303,9 @@ client.on('message', message => {
     }
             message.guild.members.forEach(m => {
        if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .addField('» السيرفر :', `${message.guild.name}`)
+                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
                 .addField(' » الرسالة : ', args)
                 .setColor('#ff0000')
                 // m.send(`[${m}]`);
