32b4af0c0ef1f58e790253e782089d72c9dca032
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-11-24 11:22:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -782,7 +782,23 @@ message.channel.sendEmbed(id);
  });
 
 
-
+client.on('message', message => {
+if(message.content.startsWith(">slots")) {
+let slot1 = ['🍏', '🍇', '🍒', '🍍', '🍅', '🍆', '🍑', '🍓'];
+let slot2 = ['🍏', '🍇', '🍒', '🍍', '🍅', '🍆', '🍑', '🍓'];
+let slot3 = ['🍏', '🍇', '🍒', '🍍', '🍅', '🍆', '🍑', '🍓'];
+let slots1 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+let slots2 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+let slots3 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+let we;
+if(slots1 === slots2 && slots2 === slots3) {
+we = "Win, GG."
+} else {
+we = "Lose :'("
+}
+message.channel.send(`${slots1} | ${slots2} | ${slots3} - ${we}`)
+}
+});
 
 
 
