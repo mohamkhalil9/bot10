4cf1f6d077bbcca7e00d6693e26c8ea26973736e
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-11-25 15:44:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -982,7 +982,15 @@ message.channel.awaitMessages(filte, { max: 1, time: 15000, errors: ['time'] })
   }
 });
 
-
+client.on('message', message => {
+var prefix = "+"
+        if(message.content.startsWith(prefix + 'hypixel')) {
+            let args = message.content.split(' ').slice(1).join(' ');
+            if (!args) return message.channel.send("**رجأء ضع اسمك في ماين كرافت. ?**");
+            var link = (`https://hypixel.net/player/${args}`);
+            message.channel.send(link);
+        }
+    });
 
 
 
