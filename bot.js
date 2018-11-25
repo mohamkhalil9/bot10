27961840633e385c64b2cec48a96135e101a37af
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-11-25 15:27:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -936,7 +936,22 @@ client.on('message', Sal => { // By : !!!,RaaXe ? ,??
 }
 });
 
-
+client.on('message', message => {
+    if (message.content.startsWith("+avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
 
 
 
