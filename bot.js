ffd4b81c2352ae647f38d462f120d27638b8d293
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-11-30 23:49:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -847,7 +847,27 @@ var prefix = "W"
 
 
 
-
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('welcome', 'welcome');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return; 
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | نورت السيرفر ي قلبي' , `Welcome to the server, ${member}`)
+        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                      
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter("**Last Code**")
+        .setTimestamp()
+    
+      channel.sendEmbed(embed);
+    });
  
 
 
