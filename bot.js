a500684e4fbb4779d249930411bb58f64ea4f3ac
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-02 18:33:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -912,6 +912,17 @@ hours = 12;
 });
 
 
+client.on('message', message => {
+              if (!message.channel.guild) return;
+      if(message.content =='Wmember')
+      var IzRo = new Discord.RichEmbed()
+      .setThumbnail(message.author.iconURL)
+      .setFooter(message.author.username, message.author.avatarURL)
+      .setTitle(' عدد اعضاء السيرفر  ')
+      .addBlankField(true)
+      .addField('.',`${message.guild.memberCount}`)
+      message.channel.send(IzRo);
+    });
 
 
 
